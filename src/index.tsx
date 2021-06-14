4d0ea0f0c263b42f8ff1eb0ef8650a6eb5d2c06f
Romain T <romain-thd+github@protonmail.com>
2021-06-14 15:25:31 +0200

    new: Font Awesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import "main.scss";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+import {fas} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import ReactDOM from "react-dom";
-
 import {
     BrowserRouter as Router,
     Route,
     Switch
 } from "react-router-dom";
 import Routes from "routes";
+import "main.scss";
+
+library.add(fab, fas);
 
 ReactDOM.render((
     <Router>
