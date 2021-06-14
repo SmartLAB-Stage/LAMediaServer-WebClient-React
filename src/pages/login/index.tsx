fc93e0d6bed16fbcf25f74ea8d2bcee83318d781
Romain T <romain-thd+github@protonmail.com>
2021-06-14 15:27:22 +0200

    fix: Passage à Bootstrap 4
    Comme conseillé dans la documentation officielle

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -77,92 +77,110 @@ export default class Login extends React.Component<LoginProps, LoginState> {
 
     public render(): React.ReactNode {
         return (
-            <div className={"text-center"}>
-                <main className={"login form-signin"}>
-                    <Form onSubmit={(e) => this._handleSubmit(e)}>
-                        <Image
-                            className={"mb-4"}
-                            src={"static/res/logo.png"}
-                            alt={"Logo"}
+            <main className={"text-center login"}>
+                <Form className={"form-signin"} onSubmit={(e) => this._handleSubmit(e)}>
+                    <Image
+                        className={"mb-4"}
+                        src={"static/res/logo.png"}
+                        alt={"Logo"}
+                    />
+                    <h1 className={"h3 mb-3 fw-normal"}>
+                        Veuillez vous connecter
+                    </h1>
+                    <Form.Group controlId={"username"} className={"form-floating"}>
+                        <Form.Control
+                            autoFocus
+                            name={"username"}
+                            type={"text"}
+                            placeholder={"Nom d'utilisateur"}
+                            defaultValue={this.state.username}
+                            onChange={
+                                (e) => this.setState({
+                                    username: e.target.value,
+                                    status: LoginStatus.NONE,
+                                })
+                            }
                         />
-                        <h1 className={"h3 mb-3 fw-normal"}>
-                            Veuillez vous connecter
-                        </h1>
-                        <Form.Group controlId={"username"} className={"form-floating"}>
-                            <Form.Control
-                                autoFocus
-                                name={"username"}
-                                type={"text"}
-                                placeholder={"username"}
-                                defaultValue={this.state.username}
+                        <Form.Label
+                            className={"sr-only"}>
+                            Nom d'utilisateur
+                        </Form.Label>
+                    </Form.Group>
+                    <Form.Group controlId={"password"} className={"form-floating"}>
+                        <Form.Control
+                            type={"password"}
+                            name={"password"}
+                            placeholder={"Mot de passe"}
+                            defaultValue={this.state.password}
+                            onChange={
+                                (e) => this.setState({
+                                    password: e.target.value,
+                                    status: LoginStatus.NONE,
+                                })
+                            }
+                        />
+                        <Form.Label
+                            className={"sr-only"}>
+                            Mot de passe
+                        </Form.Label>
+                    </Form.Group>
+                    <Form.Group controlId={"remember"} className={"mb-2"}>
+                        <Form.Label>
+                            <Form.Check
+                                type={"checkbox"}
+                                label={"Se souvenir de moi"}
                                 onChange={
-                                    (e) => this.setState({
-                                        username: e.target.value,
-                                        status: LoginStatus.NONE,
-                                    })
-                                }
-                            />
-                            <Form.Label>
-                                Nom d'utilisateur
-                            </Form.Label>
-                        </Form.Group>
-                        <Form.Group controlId={"password"} className={"form-floating"}>
-                            <Form.Control
-                                type={"password"}
-                                name={"password"}
-                                placeholder={"password"}
-                                defaultValue={this.state.password}
-                                onChange={
-                                    (e) => this.setState({
-                                        password: e.target.value,
-                                        status: LoginStatus.NONE,
-                                    })
-                                }
-                            />
-                            <Form.Label>
-                                Mot de passe
-                            </Form.Label>
-                        </Form.Group>
-                        <Form.Group controlId={"remember"} className={"mb-2"}>
-                            <Form.Label>
-                                <Form.Check
-                                    type={"checkbox"}
-                                    label={"Se souvenir de moi"}
-                                    onChange={
-                                        (e) => {
-                                            this.setState({remember: e.target.checked})
-                                        }
+                                    (e) => {
+                                        this.setState({remember: e.target.checked})
                                     }
-                                />
-                            </Form.Label>
-                        </Form.Group>
-                        <Button
-                            className={`w-100 btn btn-lg btn-${this.state.status}`}
-                            type={"submit"}
-                            disabled={!this._validForm()}
-                            loading={this.state.loading}
-                        >
-                            Se connecter
-                        </Button>
-                        <div
-                            className={`alert alert-${this.state.status} alert-dismissible fade show`}
-                            role={"alert"}
-                            hidden={this.state.status === LoginStatus.NONE}
-                        >{
-                            this.state.status === LoginStatus.SUCCESS ? (
-                                <strong>Succès</strong>
-                            ) : (
-                                <div>
-                                    <strong>Échec de la connexion</strong>
-                                    <br/>
-                                    <span>{this.state.errorMessage}</span>
-                                </div>
-                            )
-                        }</div>
-                    </Form>
-                </main>
-            </div>
+                                }
+                            />
+                        </Form.Label>
+                    </Form.Group>
+                    <Button
+                        className={`w-100 btn btn-lg btn-${this.state.status}`}
+                        type={"submit"}
+                        disabled={!this._validForm()}
+                        loading={this.state.loading}
+                    >
+                        Se connecter
+                    </Button>
+                    <div
+                        className={`alert alert-${this.state.status} alert-dismissible fade show`}
+                        role={"alert"}
+                        hidden={this.state.status === LoginStatus.NONE}
+                    >{
+                        this.state.status === LoginStatus.SUCCESS ? (
+                            <strong>Succès</strong>
+                        ) : (
+                            <div>
+                                <strong>Échec de la connexion</strong>
+                                <br/>
+                                <span>{this.state.errorMessage}</span>
+                            </div>
+                        )
+                    }</div>
+                </Form>
+            </main>
         );
+    }
+
+    /**
+     * Édite le CSS.
+     * Impossible de changer les propriétés du body sans le changer sur toutes les pages, d'où cette fonction
+     */
+    componentWillMount(): void {
+        document.body.style.display = "flex";
+        document.body.style.alignItems = "center";
+        document.body.style.justifyContent = "center";
+    }
+
+    componentWillUnmount(): void {
+        /*
+        document.body.style.display = null;
+        document.body.style.alignItems = null;
+        document.body.style.justifyContent = null;
+         */
     }
 
     /**
