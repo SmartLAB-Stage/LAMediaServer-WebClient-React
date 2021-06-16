25e24730de21780225ab25fa596eec86357c7787
Romain T <romain-thd+github@protonmail.com>
2021-06-16 14:40:05 +0200

    new: Protection des routes non authentifiées

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,15 @@
-import dotenv from "dotenv";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {fab} from "@fortawesome/free-brands-svg-icons";
 import {fas} from "@fortawesome/free-solid-svg-icons";
-import ReactDOM from "react-dom";
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-} from "react-router-dom";
-import {Routes} from "routes";
+import dotenv from "dotenv";
 import "main.scss";
+import {Routes} from "pages/_routes";
+import ReactDOM from "react-dom";
 
 dotenv.config();
 
 library.add(fab, fas);
 
 ReactDOM.render((
-    <Router>
-        <Switch>
-            <Route path="/" component={Routes}/>
-        </Switch>
-    </Router>
+    <Routes/>
 ), document.getElementById("root"));
