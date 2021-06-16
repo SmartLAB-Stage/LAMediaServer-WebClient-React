d94721c286e6ffd2a8d5eb32ae165ac7c162286d
Romain T <romain-thd+github@protonmail.com>
2021-06-16 14:41:45 +0200

    new: Page 404 basique

---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class NotFound extends React.Component {
+    public render(): React.ReactNode {
+        return (
+            "Cette page n'existe pas"
+        );
+    }
+}
+
+export {NotFound};
