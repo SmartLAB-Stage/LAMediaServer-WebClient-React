f401e3ceb0cdac3a265ec6b8cab8891c697ae5b0
Romain T <romain-thd+github@protonmail.com>
2021-06-21 15:47:38 +0200

    new: Tooltip au dessus des discussions

--- src/components/roomList/index.tsx
+++ src/components/roomList/index.tsx
@@ -1,9 +1,15 @@
 import {Room} from "model/room";
 import React from "react";
+import {
+    Button,
+    OverlayTrigger,
+    Tooltip,
+} from "react-bootstrap";
 import "./roomList.scss";
 
 interface RoomListProps {
     rooms: Room[],
+    currentRoomChangeCallback: (Room) => void,
 }
 
 interface RoomListState {
@@ -15,38 +21,42 @@ class RoomList extends React.Component<RoomListProps, RoomListState> {
 
         for (const room of this.props.rooms) {
             reactRooms.push(
-                <a key={room.id} href={`/room/${room.id}`}
-                   className={"list-group-item list-group-item-action list-group-item-light rounded-0"}>
-                    <div className={"media"}>
-                        <div className={"svg-align-container"}>
-                            <div className={"svg-align-center"}>
-                                <img src={"/static/res/chat-logo.svg"}
-                                     alt={"Salon"}
-                                     width={"100%"}
-                                />
+                <OverlayTrigger key={room.id}
+                                placement="top"
+                                overlay={(props) => this._renderTooltip(props, room)}>
+                    <Button onClick={() => this.props.currentRoomChangeCallback(room)}
+                            className={"button list-group-item list-group-item-action list-group-item-light rounded-0"}>
+                        <div className={"media"}>
+                            <div className={"svg-align-container"}>
+                                <div className={"svg-align-center"}>
+                                    <img src={"/static/res/chat-logo.svg"}
+                                         alt={"Salon"}
+                                         width={"100%"}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <div className={"media-body ml-4"}>
-                            <div className={"d-flex align-items-center justify-content-between mb-1"}>
-                                <h6 className={"mb-0"}>
-                                    {room.name}
-                                </h6>
-                                <small className={"small font-weight-bold"}>
+                            <div className={"media-body ml-4"}>
+                                <div className={"d-flex align-items-center justify-content-between mb-1"}>
+                                    <h6 className={"mb-0"}>
+                                        {room.name}
+                                    </h6>
+                                    <small className={"small font-weight-bold"}>
+                                        {room.lastMessage !== undefined
+                                            ? room.lastMessage.parentUser.name
+                                            : <i>Expéditeur inconnu</i>
+                                        }
+                                    </small>
+                                </div>
+                                <p className={"text-muted mb-0 text-small"}>
                                     {room.lastMessage !== undefined
-                                        ? room.lastMessage.parentUser.name
-                                        : <i>Expéditeur inconnu</i>
+                                        ? room.lastMessage.content
+                                        : <i>Dernier message non disponible</i>
                                     }
-                                </small>
+                                </p>
                             </div>
-                            <p className={"text-muted mb-0 text-small"}>
-                                {room.lastMessage !== undefined
-                                    ? room.lastMessage.content
-                                    : <i>Dernier message non disponible</i>
-                                }
-                            </p>
                         </div>
-                    </div>
-                </a>
+                    </Button>
+                </OverlayTrigger>
             );
         }
 
@@ -55,6 +65,10 @@ class RoomList extends React.Component<RoomListProps, RoomListState> {
                 {reactRooms}
             </div>
         );
+    }
+
+    private _renderTooltip(props, room: Room): React.ReactNode {
+        return <Tooltip {...props}>{room.name}</Tooltip>;
     }
 }
 
