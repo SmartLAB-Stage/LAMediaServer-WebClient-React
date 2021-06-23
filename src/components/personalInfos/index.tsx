4eb78fe77162b0e65631544eea5a44721fa33828
Romain T <romain-thd+github@protonmail.com>
2021-06-23 11:44:43 +0200

    new: Photo de profil dans les informations personnelles

--- src/components/personalInfos/index.tsx
+++ src/components/personalInfos/index.tsx
@@ -1,5 +1,7 @@
+import {ProfilePicture} from "components/profilePicture";
 import {User} from "model/user";
 import React from "react";
+import "./personalInfos.scss";
 
 interface PersonalInfosProps {
     user: User | null,
@@ -8,7 +10,10 @@ interface PersonalInfosProps {
 class PersonalInfos extends React.Component<PersonalInfosProps, {}> {
     public render(): React.ReactNode {
         return (
-            <div className="card border-dark mb-3">
+            <div className="card border-secondary mb-0 flex-row flex-wrap">
+                <div className="card-header border-0">
+                    <ProfilePicture user={this.props.user}/>
+                </div>
                 <div className="card-body text-dark">
                     <p className="card-text">
                         {this.props.user === null
